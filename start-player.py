2297582f7c8603945984d4deaf4080782ba4197f
Michal Mašek <misamasek@gmail.com>
2016-04-02 18:00:24 +0200

    Choose also the order of players

--- start-player.py
+++ start-player.py
@@ -8,12 +8,11 @@ parser = argparse.ArgumentParser(description='Choose color for each player and t
 parser.add_argument('players', metavar='PLAYER', nargs='+', help='Who is playing?')
 args = parser.parse_args()
 
+random.shuffle(args.players)
+
 remaining_colors = ["Brown", "Black", "Blue", "Green", "Gray", "Red", "Yellow"]
 
 for player in args.players:
     color = random.choice(remaining_colors)
     remaining_colors.remove(color)
     print "%s -> %s" % (player, color)
-
-starting_player = random.choice(args.players)
-print "%s starts" % starting_player
